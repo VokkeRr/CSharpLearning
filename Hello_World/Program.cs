7fd656678341f25c660944a37cae2bc662440ee1
Vokker <turboruslan.com@gmail.com>
2022-11-13 23:19:33 +0600

    Console.write
    Here i learned simple comands of System library/// like System.Console.Write = write sth in console.

--- Hello_World/Program.cs
+++ Hello_World/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Hello_World
+{
+    class Program{
+
+        // this is coment (on one line(Ctrl+/))
+        /*
+        This is coment (Until closing)
+        */
+
+        static void Main() {
+            Console.WriteLine("Hello MQ");
+            Console.Write("Hello in one line");
+            Console.Write("Hello in line");
+            Console.Write("Hello in one line \n");
+            Console.Write("Hello in line");
+        }
+    }
+}
