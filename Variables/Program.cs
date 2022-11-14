4078792f966f183c3140c991cb0fcbf3efdbad20
Vokker <turboruslan.com@gmail.com>
2022-11-15 01:01:07 +0600

    Start

---
+++ Variables/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project{
+
+    class Program{
+
+        static void Main() {
+            int num_1 = 0;
+
+            num_1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Answer " + num_1);
+        }
+    }
+}
