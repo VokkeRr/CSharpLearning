bd914d16ff61be52da4f1cc193e8b477ed9a4334
Vokker <turboruslan.com@gmail.com>
2022-11-15 01:10:27 +0600

    Some changes

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -5,9 +5,14 @@ namespace Project{
     class Program{
 
         static void Main() {
-            int num_1 = 0;
+            const float PI = 3.14159265f; // Константа, неизменяеммое значение.
+            int num_1,box=3;
+            
+            box -= 2; // оператор присваивания box = box - 2 (3-2=1)
 
             num_1 = Convert.ToInt32(Console.ReadLine());
+            num_1 *= 5; // оператор присваивания num_1= num_1 * 5
+
 
             Console.WriteLine("Answer " + num_1);
         }
