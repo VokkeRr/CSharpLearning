0837d0c56990e1452e4b7212ceff36bcbf1ac760
Vokker <turboruslan.com@gmail.com>
2022-11-16 21:33:47 +0600

    Update 2.1
    NEW update

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -13,6 +13,16 @@ namespace Project{
             num_1 = Convert.ToInt32(Console.ReadLine());
             num_1 *= 5; // оператор присваивания num_1= num_1 * 5
 
+            int a = 45;
+            double b = 34.56;
+
+            a=(int)b;  // происходит потеря дробной части; 
+            a=(int)Math.Round(b); // МЕТОД Round класса Math округлаят число 3.5=4 3.3=3
+
+
+            double de = 12.23; // по умолчанию все числа с плавающей точкой, типа double;
+            float df = 12.24f; // f служит подтверждение на перевод в тип float // float df=(float)12.24 
+
 
             Console.WriteLine("Answer " + num_1);
         }
